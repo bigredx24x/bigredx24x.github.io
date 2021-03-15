088a11e56910a7840376a58153718a31c5928d90
anthony <aj.go.bucks@gmail.com>
2021-03-14 23:03:37 -0400

    2021 update

--- js/bracket_2021.js
+++ js/bracket_2021.js
@@ -4,6 +4,11 @@ for(i=0; i<dataSet.length; i++){
   dataSet[i]['projection'] = 0;
 }
 
+stats_config = {pfp: 1,
+                pfr: 0,
+                pfa: 0,
+                pfb: 0}; 
+
 
 function pick_winner(elem){
   // id will be <round>_<game><top/bottom>
@@ -51,7 +56,9 @@ function update_data_stats(ngames_by_team){
       if (dataSet[i]['team_name'] === team){
         found_team = true;
         dataSet[i]['ngames'] = ngames_by_team[team]
-        dataSet[i]['projection'] = (ngames_by_team[team] * parseFloat(dataSet[i]['PPG'])).toFixed(2)
+        // TODO - update this per stats_config
+        // AST, STL, REB, PTS, TO
+        dataSet[i]['projection'] = (ngames_by_team[team] * parseFloat(dataSet[i]['PTS'])).toFixed(2)
         break
       }
     }
@@ -66,84 +73,84 @@ function redraw_table(){
   $('#example').dataTable().fnAddData(dataSet);
 }
 
-var teamsff = ['16 Prarie View',
-                  '16 F Dickinson',
-                  '11 Belmont',
-                  '11 Temple',
-                  '16 NDSU',
-                  '16 Nc Central',
-                  '11 Arizona State',
-                  '11 St Johns']
+var teamsff = ['16 Norfolk St',
+                  '16 Appalachian State',
+                  '11 Wichita State',
+                  '11 Drake',
+                  '16 Mount St. Mary\'s',
+                  '16 Texas Southern',
+                  '11 Michigan State',
+                  '11 UCLA']
 
-var ff_map2game = [8,4,0,12]
+var ff_map2game = [0,4,8,12]
 
-var teams = ['1 Duke',
-             'ff3',
-             '8 VCU',
-             '9 UCF',
-             '5 Mississippi State',
-             '12 Liberty',
-             '4 Virginia Tech',
-             '13 Saint Louis',
-             '6 Maryland',
-             'ff2',
-             '3 LSU',
-             '14 Yale',
-             '7 Louisville',
-             '10 Minnesota',
-             '2 Michigan State',
-             '15 Bradley',
-             // ---
-             '1 Gonzaga',
+var teams = ['1 Gonzaga',
              'ff1',
-             '8 Syracuse',
-             '9 Baylor',
-             '5 Marquette',
-             '12 Murray State',
+             '8 Oklahoma',
+             '9 Missouri',
+             '5 Creighton',
+             '12 UC Santa Barbara',
+             '4 Virginia',
+             '13 Ohio',
+             '6 USC',
+             'ff2',
+             '3 Kansas',
+             '14 Eastern Washington',
+             '7 Oregon',
+             '10 VCU',
+             '2 Iowa',
+             '15 Grand Canyon',
+             // ---
+             '1 Michigan',
+             'ff3',
+             '8 LSU',
+             '9 St. Bonaventure',
+             '5 Colorado',
+             '12 Georgetown',
              '4 Florida State',
-             '13 Vermont',
-             '6 Buffalo',
+             '13 UNC Greensboro',
+             '6 BYU',
              'ff4',
-             '3 Texas Tech',
-             '14 Northern Kentucky',
-             '7 Nevada',
-             '10 Florida',
-             '2 Michigan',
-             '15 Montana',
+             '3 Texas',
+             '14 Abilene Christian',
+             '7 UConn',
+             '10 Maryland',
+             '2 Alabama',
+             '15 Iona',
              // ---
-             '1 Virginia',
-             '16 Gardner-Webb',
-             '8 Ole Miss',
-             '9 Oklahoma',
-             '5 Wisconsin',
-             '12 Oregon',
-             '4 Kansas State',
-             '13 UC Irvine',
-             '6 Villanova',
-             '11 Saint Mary\'s',
-             '3 Purdue',
-             '14 Old Dominion',
-             '7 Cincinnati',
-             '10 Iowa',
-             '2 Tennessee',
-             '15 Colgate',
+             '1 Baylor',
+             '16 Hartford',
+             '8 North Carolina',
+             '9 Wisconsin',
+             '5 Villanova',
+             '12 Winthrop',
+             '4 Purdue',
+             '13 North Texas',
+             '6 Texas Tech',
+             '11 Utah State',
+             '3 Arkansas',
+             '14 Colgate',
+             '7 Florida',
+             '10 Virginia Tech',
+             '2 Ohio State',
+             '15 Oral Roberts',
              // ---
-             '1 North Carolina',
-             '16 Iona',
-             '8 Utah State',
-             '9 Washington',
-             '5 Auburn',
-             '12 New Mexico State',
-             '4 Kansas',
-             '13 Northeastern',
-             '6 Iowa State',
-             '11 Ohio State',
-             '3 Houston',
-             '14 Georgia State',
-             '7 Wofford',
-             '10 Seton Hall',
-             '2 Kentucky',
-             '15 Abilene Christian']
+             '1 Illinois',
+             '16 Drexel',
+             '8 Loyola Chicago',
+             '9 Georgia Tech',
+             '5 Tennessee',
+             '12 Oregon State',
+             '4 Oklahoma State',
+             '13 Liberty',
+             '6 San Diego State',
+             '11 Syracuse',
+             '3 West Virginia',
+             '14 Morehead State',
+             '7 Clemson',
+             '10 Rutgers',
+             '2 Houston',
+             '15 Cleveland State']
 var ngames_by_team = {};
 
 document.addEventListener("DOMContentLoaded", load_tourney());
@@ -180,6 +187,16 @@ function load_tourney(){
 
   fill_first_four();
   fill_round_1();
+}
+
+function stats_config_change(){
+  console.log("stats_config_change...");
+  stats_config.pfp = document.getElementById("pfp").value;
+  stats_config.pfr = document.getElementById("pfr").value;
+  stats_config.pfa = document.getElementById("pfa").value;
+  stats_config.pfb = document.getElementById("pfb").value;
+  console.log(stats_config);
+
 }
 
 function get_empty_game_elements(ngames, round, offset){
@@ -262,12 +279,13 @@ $(document).ready(function() {
     var table = $('#example').DataTable( {
         data: dataSet,
         columns: [
-            { data : 'Player' , title : 'Player'},
+            { data : 'Name' , title : 'Player'},
             { data : 'team_name', title : 'Team'},
             { data : 'team_rank' , title : 'Rank'},
-            { data : 'PPG' , title : 'PPG'},
-            { data : 'ngames', title : '# Games'},
-            { data : 'projection', title : 'Projection'},
+            { data : 'GP' , title : 'Games Played'},
+            { data : 'PTS' , title : 'PPG'},
+            { data : 'ngames', title : 'Projected Games'},
+            { data : 'projection', title : 'Projected Pts'},
         ]
     } );
     table.order([ 5, "desc" ]).draw()
